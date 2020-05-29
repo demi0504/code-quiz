775d758e97dc2582073642bd48503af9919ccea4
demi0504 <ripley.demi@gmail.com>
2020-05-29 18:58:37 -0500

    added styling to start page elements

---
+++ script.js
@@ -0,0 +1,10 @@
+const startButton = document.querySelector("#startBtn");
+const countdown = document.querySelector("#timer");
+const question = document.querySelector("#question");
+const choices = document.querySelector("#choices");
+const choiceA = document.querySelector("#a");
+const choiceB = document.querySelector("#b");
+const choiceC = document.querySelector("#c");
+const totalScore = document.querySelector("#scoreContainer");
+const intialsBox = document.querySelector("#initialsForm");
+
