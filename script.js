27d691e5d4f4be02c684ef3974e2b78fe67a81e3
demi0504 <ripley.demi@gmail.com>
2020-06-01 18:29:35 -0500

    timer function and next questions functions running

--- script.js
+++ script.js
@@ -1,91 +1,115 @@
-
-
+//HTML elements
 const startButton = document.querySelector("#startBtn");
 const beginQuizEl = document.querySelector(".beginQuiz");
 const countdown = document.querySelector("#timer");
 const questionContainer = document.querySelector(".question-container");
 const choices = document.querySelector(".choice");
 const question = document.querySelector("#question");
-const choiceA = document.querySelector("#a");
-const choiceB = document.querySelector("#b");
-const choiceC = document.querySelector("#c");
-const totalScore = document.querySelector("#scoreContainer");
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+const totalScore = document.querySelector(".score-container");
 const intialsBox = document.querySelector("#initialsForm");
 
 //object of questions and answers
 const myQuestions = [
     {
         question: "How many computer programming languages are there?",
-            a: "Under 50",
-            b: "Roughly 400",
-            c: "Over 700",
-            correctAnswer: 'c'
+            choiceA: "Under 50", 
+            choiceB: "Roughly 400", 
+            choiceC: "Over 700", 
+            correctAnswer: "C"
     },
     {
         question: "Who was the first computer programmer?",
-            a: "Bill Gates",
-            b: "Ada Lovelace",
-            c: "Niklaus Wirth",
-            correctAnswer: 'b'
+            choiceA: "Bill Gates", 
+            choiceB: "Ada Lovelace", 
+            choiceC: "Niklaus Wirth", 
+            correctAnswer: "B"
     }, 
     {
         question: "What are semantic elements?",
-            a: "A semantic element clearly describes its meaning to both the browser and the developer.",
-            b: "In HTML this refers to div tags.",
-            c: "They help to style certain HTML tags.",
-            correctAnswer: 'a'
+            choiceA: "A semantic element clearly describes its meaning to both the browser and the developer.",
+            choiceB: "In HTML this refers to div tags.", 
+            choiceC: "They help to style certain HTML tags.", 
+            correctAnswer: "A"
     }
 ];
+//Variables for functions
+const lastQuestion = myQuestions.length - 1;
+let runningQuestion = 0;
+let endTime = 0;
+let count = 30;
+let timer;
+let score = 0;
 
-let shuffledQuestions, currentQuestionIndex;
-let quizTime = 90;
+//Function to show next question
+function showQuestion() {
+    let q = myQuestions[runningQuestion];
+    question.innerHTML = q.question;
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+}
 
 //Function for start button to initiate quiz questions
 startButton.addEventListener('click', startQuiz);
 function startQuiz() {
-    // console.log('started');
     startButton.classList.add('hide');
     beginQuizEl.classList.add('hide');
-    shuffledQuestions = myQuestions.sort(() => Math.random() - .5);
-    currentQuestionIndex = 0;
     questionContainer.classList.remove('hide');
-    nextQuestion();
-    // timerBegin();
-}
-
-//Function to show next question
-function nextQuestion() { 
-    showQuestion(shuffledQuestions[currentQuestionIndex]); 
-}
-
-function showQuestion() {
-    question.innerHTML = myQuestions[0].question;
-    choiceA.innerHTML = myQuestions[0].a;
-    choiceB.innerHTML = myQuestions[0].b;
-    choiceC.innerHTML = myQuestions[0].c;
-    currentQuestionIndex++;
-    // nextQuestion();
+    showQuestion();
+    renderTimer();
+    timer = setInterval(renderTimer, 1000);
 }
 
 //Timer function
-// function timerBegin() {
-//     timer.innerHTML = quizTime;
-//     for ()
-// }
-//check answer
+function renderTimer() {
+    if (count >= endTime) {
+        countdown.innerHTML = count;
+        count--
+    } else {
+        count = 0;
+        answerIsWrong();
+        if (runningQuestion < lastQuestion) {
+            runningQuestion++;
+            showQuestion();
+        } else {
+            clearInterval(timer);
+            finalScore();
+        }
+    }
+}
+
+//Check answer
 function checkAnswer(answer) {
-    if(answer === myQuestions[shuffledQuestions].correctAnswer){
-        totalScore++;
+    if (answer == myQuestions[runningQuestion].correctAnswer) {
+        score++
         answerIsCorrect();
     } else {
         answerIsWrong();
     }
+    if(runningQuestion < lastQuestion) {
+        runningQuestion++;
+        showQuestion();
+    } else {
+        clearInterval(timer);
+        finalScore();
+    }
 }
 
 function answerIsCorrect() {
-    choices.style.backgroundColor = "#3CB371";
+    console.log("youre right");
 }
 
 function answerIsWrong() {
-    choices.style.backgroundColor = "#FF0000";
+    console.log("youre wrong");
+}
+
+//Final score percentage
+function finalScore(){
+    questionContainer.classList.add('hide');
+    totalScore.classList.remove('hide');
+    const scorePercent = Math.round(100 * score/myQuestions.length);
+    totalScore.innerHTML += "<p>"+ scorePercent +"</p>";
 }
